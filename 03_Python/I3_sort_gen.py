5484ace375d88d0991453ed96a7e7eb57fb55774
monica0325 <b09901046@ntu.edu.tw>
2023-05-23 16:36:32 +0800

    add all file

---
+++ 03_Python/I3_sort_gen.py
@@ -0,0 +1,24 @@
+def sort(v, n):
+    for i in range(n):
+        for j in range(i-1,-1,-1):
+            if v[j] > v[j+1]:
+                v[j], v[j+1] = v[j+1], v[j]
+    return v
+
+
+
+if __name__ == '__main__':
+    # Modify your test pattern here
+    n = 5
+    v = [3,1,5,2,4]
+
+    with open('../00_TB/Pattern/I3/mem_D.dat', 'w') as f_data:
+        f_data.write(f"{n:08x}\n")
+        for ele in v:
+            f_data.write(f"{ele:08x}\n")
+
+
+    with open('../00_TB/Pattern/I3/golden.dat', 'w') as f_ans:
+        f_ans.write('{:0>8x}\n'.format(n))
+        for item in sort(v, n):
+            f_ans.write('{:0>8x}\n'.format(item))
